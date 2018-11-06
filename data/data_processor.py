e73df4b01766b01fc6d901e6f1046a7765b83009
Jernej Vivod <je.vivod@gmail.com>
2018-11-06 22:59:29 +0100

    Add grouping by bus line

--- data/data_processor.py
+++ data/data_processor.py
@@ -1,3 +1,6 @@
+import numpy as np
+import csv
+import multiprocessing
 """
 Ideas for feature creation:
 date -> hours, minutes, seconds
@@ -9,19 +12,26 @@ class data_processor:
 
     # Constructor: initialize instance with file containing the data.
     def __init__(self, data_file):
-        self.data = data_file
+        # Create matrix out of
+        reader = csv.reader(open(data_file, "r"), delimiter="\t")
+        x = list(reader)
+        self.data_matrix = np.array(x)
         self.bus_line_matrices = None
 
-    # _extract_line_numbers: extract all bus line numbers found in csv data file.
-    def _extract_line_numbers(self):
-        # TODO
-        pass
 
     # _get_bus_line_matrices: make a dictionary that maps each bus line to a matrix of its
     # features obtained from the csv file.
     def _get_bus_line_matrices(self):
-        # TODO
-        pass
+
+        # add_line_row: add line in data matrix as value for corresponding key in dictionary.
+        def add_line_row(x):
+            self.bus_line_matrices[x[2]] = np.vstack((self.bus_line_matrices[x[2]], x))
+
+        # Get unique bus lines and make a dictionary that maps bus lines to empty row vector.
+        self.bus_line_matrices = dict.fromkeys(np.unique(self.data_matrix[1:, 2]), np.array([], dtype=np.character).reshape(0, self.data_matrix.shape[1]))
+        # Get key values by applying the add_line_row function to each row in training matrix.
+        np.apply_along_axis(add_line_row, axis=1, arr=self.data_matrix[1:,:])
+
 
     # _decompose_features: construct new features from the composite features found in the data file.
     def _decompose_features(self):
